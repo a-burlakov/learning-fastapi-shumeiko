d2e21b281b6a2d8407f4022a46b526cc42ab0f9a
a-burlakov <fln@mail.ru>
2023-04-07 16:17:51 +0500

    lesson: fastapi-users integration.

--- main.py
+++ main.py
@@ -4,13 +4,36 @@ from typing import List, Optional
 
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
+from fastapi_users import fastapi_users, FastAPIUsers
 from pydantic import BaseModel, Field, ValidationError
 from starlette import status
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
+from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+
 app = FastAPI(
     title="Trading App",
+)
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
 )
 
 
