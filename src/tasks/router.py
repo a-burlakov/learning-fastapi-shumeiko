c83114a5519cd9f7f5df1c03c48e94f88d496c79
a-burlakov <fln@mail.ru>
2023-04-10 17:39:48 +0500

    lesson: code from lesson.

---
+++ src/tasks/router.py
@@ -0,0 +1,53 @@
+from asyncio import sleep
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi_cache.decorator import cache
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.encoders import jsonable_encoder
+
+from src.auth.schemas import Operation
+from src.database import get_async_session
+from src.operations.models import operation
+
+router = APIRouter(
+    prefix="/operations",
+    tags=["Operations"],
+)
+
+
+@router.get(
+    "/",
+    response_model=List[Operation],
+)
+@cache(expire=2)
+async def get_specific_operations(
+    operation_type: str, session: AsyncSession = Depends(get_async_session)
+):
+    await sleep(1)
+    # Желательно всегда ставить exception, потому что все может пойти не так.
+    try:
+        query = select(operation).where(operation.c.type == operation_type)
+
+        result = await session.execute(query)
+        resres = result.all()
+        # x = 1 / 0
+        print(resres)
+        return resres
+    except ZeroDivisionError:
+        return HTTPException(
+            status_code=500,
+            detail={
+                "status": "dividing to zero",
+                "data": None,
+                "details": "fatal overkill",
+            },
+        )
+
+    except:
+        return {
+            "status": "error",
+            "data": None,
+            "details": None,
+        }
