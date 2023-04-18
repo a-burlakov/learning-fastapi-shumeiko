8f1976f7ae226ad5a6fec2b2a2d605e708aa5c41
a-burlakov <fln@mail.ru>
2023-04-18 11:54:32 +0500

    lesson: "Depends" learning. Using via class.

--- src/main.py
+++ src/main.py
@@ -10,6 +10,19 @@ async def get_async_session():
     print("Уничтожение сессии")
 
 
+def pagination_params(limit: int = 10, skip: int = 0):
+    return {
+        "limit": limit,
+        "skip": skip,
+    }
+
+
+class Paginator:
+    def __init__(self, limit: int = 10, skip: int = 0):
+        self.limit = limit
+        self.skip = skip
+
+
 # depends - временные соединения с БД, redis и т.д.
 # мы сначала получаем сессию, она в ендпоинт отдается, и только потом контроль в get_async_session возвращается, и сессия уничтожается
 # если, кстати, в get_async_session поместить парамтер, то он будет требоваться при посылании сигнала на view
@@ -21,6 +34,14 @@ async def get_items(session=Depends(get_async_session)):
     return [{"id": 1}]
 
 
+# FastAPI, перед тем, как обрабатывать запрос опользователя, он все зависимости прогоняет.
+# То есть берет функцию из Depends, и говорит: pagination_params = pagination_params()
 @app.get("/subjects")
-async def get_subjects(limit: int = 10, skip: int = 0):
+async def get_subjects(pagination_params: dict = Depends(pagination_params)):
+    return [{"id": 1}]
+
+
+# В Depends также можно прокидывать и создание экземпляра класса, а не только функции.
+@app.get("/subjects_class")
+async def get_subjects(pagination_params: Paginator = Depends(Paginator)):
     return [{"id": 1}]
