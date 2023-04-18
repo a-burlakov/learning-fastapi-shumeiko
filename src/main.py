dd1d6ca16c71465a9634889383df6806feea7ea9
a-burlakov <fln@mail.ru>
2023-04-18 12:10:30 +0500

    lesson: deeper "Depends" learning.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
+from starlette.requests import Request
 
 app = FastAPI()
 
@@ -34,6 +35,21 @@ async def get_items(session=Depends(get_async_session)):
     return [{"id": 1}]
 
 
+class AuthGuard:
+    def __init__(self, name: str):
+        pass
+
+    def __call__(self, request: Request):
+        # return True
+        if "super_cookie" not in request.cookies:
+            raise HTTPException(status_code=403, detail="Запрещено")
+        else:
+            # Проверяем, что в куках есть инфа о наличии прав
+            return True
+
+
+auth_guard_payments = AuthGuard("payments")
+
 # FastAPI, перед тем, как обрабатывать запрос опользователя, он все зависимости прогоняет.
 # То есть берет функцию из Depends, и говорит: pagination_params = pagination_params()
 @app.get("/subjects")
@@ -45,3 +61,21 @@ async def get_subjects(pagination_params: dict = Depends(pagination_params)):
 @app.get("/subjects_class")
 async def get_subjects(pagination_params: Paginator = Depends(Paginator)):
     return [{"id": 1}]
+
+
+# Мы устанавливаем через Depends, что
+# енд поинт имеет какую-то зависимость.
+# Перед прогоном функции FastApi сначала прогонит все зависимости и убедится, что они не вызовут ошибки.
+# Параметр в Depends должен быть вызываемый. В нем есть метод __call__.
+# таким образом, мы сделали как бы кастомную вализацию.
+
+
+# Можно помещать в параметр
+# @app.get("/payments")
+# def get_payments(auth_guard_payments: AuthGuard = Depends(auth_guard_payments)):
+#     return "my payments"
+
+# Если в параметре зависимости не нужны, то помещаем их прям в endpoint в аргумент
+@app.get("/payments", dependencies=[Depends(auth_guard_payments)])
+def get_payments():
+    return "my payments"
